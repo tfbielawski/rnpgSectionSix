f5cd5259b5d2eb636337f73aca4a81fc0dcbcb55
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2023-11-14 15:11:52 -0500

    Secion 6: 90-94, setting up the grid, installing react navication

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoriesScreen from "./screens/CategoriesScreen.js";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <>
+        <StatusBar style={"light"}/>
+        <CategoriesScreen />
+      </>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
