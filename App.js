5d82488017b7f5635dca3194a109f9b1ec13872f
Tom Bielawski <70349084+tfbielawski@users.noreply.github.com>
2023-12-08 13:44:28 -0500

    Section 6: 90-94, assorted build files

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from "./screens/CategoriesScreen.js";
+import { NavigationContainer } from "@react-navigation/native"
 
 export default function App() {
   return (
       <>
         <StatusBar style={"light"}/>
-        <CategoriesScreen />
+          <NavigationContainer>
+              <CategoriesScreen />
+          </NavigationContainer>
+ccvv
       </>
 
   );
